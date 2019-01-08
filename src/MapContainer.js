547087b0eba753016bf16d8272cfc26a9681c6da
Alex Brinkman <abrink@commercekitchen.com>
2019-01-07 18:03:01 -0700

    [CHG] Initial commit, from create-react-app

---
+++ src/MapContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Map Container</div>;
+  }
+}
+
+export default MapContainer;
---
+++ src/ScoreContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ScoreContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Score Container</div>;
+  }
+}
+
+export default ScoreContainer;
